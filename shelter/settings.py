16f7fb1bc5623b5cce4bf63ae7a7b949469175ee
Coopermakc <Coopermakc@mail.ru>
2020-10-10 10:50:06 +0300

    static

--- shelter/settings.py
+++ shelter/settings.py
@@ -135,7 +135,10 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR, 'static')
+]
 # Media files
 
 MEDIA_URL = '/media/'
